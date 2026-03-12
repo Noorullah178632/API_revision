e45a2eb3a4a6670f7fbdfe280d5876f14dfe3755
Noor Ullah <nooullah178632@gmail.com>
2026-03-12 12:34:06 +0500

    new data

--- lib/data/network/network_api_services.dart
+++ lib/data/network/network_api_services.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:api_revision/data/appException/app_exceptions.dart';
 import 'package:api_revision/data/network/base_api_services.dart';
@@ -6,25 +7,45 @@ import 'package:http/http.dart' as http;
 
 class NetworkApiServices extends BaseApiServices {
   @override
-  getGetApiServices(String url) {}
+  getGetApiServices(String url) async {
+    dynamic jsonDecode;
+    try {
+      final response = await http
+          .get(Uri.parse(url))
+          .timeout(const Duration(seconds: 10));
+      jsonDecode = returnResponse(response);
+    } on SocketException {
+      throw FetchDataException("No internet connection");
+    }
+    return jsonDecode;
+  }
 
   @override
-  getPostApiServices(String url, data) {}
+  getPostApiServices(String url, data) async {
+    dynamic jsonDecode;
+    try {
+      final response = await http
+          .post(Uri.parse(url), body: data)
+          .timeout(const Duration(seconds: 10));
+      jsonDecode = returnResponse(response);
+    } on SocketException {
+      throw FetchDataException("No internet Exception");
+    }
+    return jsonDecode;
+  }
 
   //make a dynamic function to handle the exception of status code
   dynamic returnResponse(http.Response response) {
     switch (response.statusCode) {
       case 200:
-        dynamic jsonResponse = jsonDecode(response.body);
-        return jsonResponse;
       case 201:
-        dynamic jsonResponse = jsonDecode(response.body);
-
-        return jsonResponse;
+        return jsonDecode(response.body);
       case 400:
         return BadRequestException(response.body.toString());
+      case 401:
+      case 403:
+        throw UnauthorizedException(response.body.toString());
       case 500:
-        return FetchDataException("Server Error :");
       default:
         throw FetchDataException("Error :${response.statusCode}");
     }
